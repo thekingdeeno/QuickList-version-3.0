7c366d98872aa57d3c81d222a5bd0582d7aee4ff
Nwando Ifezue <nwandoifezue@gmail.com>
2024-02-16 15:31:49 +0100

    Updated the env interface and changed the styling a litte

--- src/process-env.d.ts
+++ src/process-env.d.ts
@@ -2,8 +2,14 @@ declare global {
     namespace NodeJS {
       interface ProcessEnv {
         [key: string]: string | undefined;
-        PORT: Number;
-        DATABASE_URL: string;
+        PORT: number;
+        HOST: string;
+        PGHOSST: string;
+        PGUSER: string;
+        PGDATABASE: string;
+        PGPASSWORD: string;
+        PGPORT: number;
+        NGROK_AUTHTOKEN: string;
       }
     }
   }
