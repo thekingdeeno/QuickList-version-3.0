f194b8e860b299ed36d9e763ac87f0ed9ff4d299
Nwando Ifezue <nwandoifezue@gmail.com>
2024-02-19 14:36:33 +0100

    Added ssl for online db hosting

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,7 @@
 import pkg from "pg";
 const {Pool} = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new Pool({
     user: process.env.PGUSER,
@@ -7,6 +9,7 @@ const pool = new Pool({
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: Number(process.env.PGPORT),
+    ssl: true
 });
 
 export default pool
